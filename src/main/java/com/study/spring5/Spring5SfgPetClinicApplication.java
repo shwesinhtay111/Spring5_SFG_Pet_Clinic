7e0a96378bfc571810e1ae1c67315dce271c663e
ShweSinHtay <shwesin@isgm2.com>
2020-05-18 19:15:25 +0630

    sfg pet clinic project in course2

---
+++ src/main/java/com/study/spring5/Spring5SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5SfgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/spring5/Spring5SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
